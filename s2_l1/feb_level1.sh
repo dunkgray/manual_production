74f451f0a611134d53a51bf4c9f2cacb65849c19
Duncan Gray <duncan.gray@ga.gov.au>
2020-04-02 13:41:55 +1100

    Adding more level 1 scripts

--- s2_l1/feb_level1.sh
+++ s2_l1/feb_level1.sh
@@ -7,20 +7,21 @@
 #PBS -l walltime=6:00:00,mem=4GB,jobfs=50GB,other=pernodejobfs
 #PBS -l wd
 #PBS -l storage=gdata/v10+scratch/v10+gdata/if87+gdata/fj7+scratch/fj7+scratch/u46+gdata/u46
-#PBS -l ncpus=18 # This value should be $END-$START+1
+#PBS -l ncpus=29 # This value should be $END-$START+1
 
 
 INPUT_DIR='/g/data/fj7/Copernicus/Sentinel-2/MSI/L1C/2019/2019-12/'
 OUTPUT_DIR='/g/data/v10/AGDCv2/indexed_datasets/cophub/s2/s2_l1c_yamls'
 
-START=14
-END=31
+START=1
+END=29
 PARA="$(($END-$START+1))"
+echo $PARA    # set this value in line 10 above!
 for i in $(seq  $START $END);
     # DAY_START DAY_END $INPUT_DIR $OUTPUT_DIR
-    do echo 2019-12-$i 2019-12-$(($i+1)) $INPUT_DIR $OUTPUT_DIR ;
+    do echo 2020-02-$i 2020-02-$(($i+1)) $INPUT_DIR $OUTPUT_DIR ;
 #done | xargs -P$PARA -n 4 ./repeat.sh
 done | xargs -P$PARA -n 4 ./level1.sh
-echo ncpus=$PARA    # set this value in line 10 above!
 
+echo ncpus=$PARA    # set this value in line 10 above!
 
--- s2_l1/level1.sh
+++ s2_l1/level1.sh
@@ -16,6 +16,18 @@ if [ "$END" = "2019-12-32" ]; then
     END=2020-01-01
 fi
 
+if [ "$END" = "2020-01-32" ]; then
+    END=2020-02-01
+fi
+
+if [ "$END" = "2020-02-30" ]; then
+    END=2020-03-01
+fi
+
+if [ "$END" = "2020-03-32" ]; then
+    END=2020-04-01
+fi
+
 echo $START $END
 echo $INPUT_DIR $OUTPUT_DIR
 s2-nci-processing generate-level1 --level1-root "$INPUT_DIR" --copy-parent-dir-count 1 --output-dir  "$OUTPUT_DIR" --start-date $START  --end-date $END
