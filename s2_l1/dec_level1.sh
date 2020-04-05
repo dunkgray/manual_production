ed2ec1ea1d7f7e85927020343a0112a3ec101f81
Duncan Gray <duncan.gray@ga.gov.au>
2020-04-06 07:54:39 +1000

    script fixes

--- s2_l1/dec_level1.sh
+++ s2_l1/dec_level1.sh
@@ -4,7 +4,7 @@
 #PBS -P u46
 #PBS -W umask=017
 #PBS -q normal
-#PBS -l walltime=6:00:00,mem=4GB,jobfs=50GB,other=pernodejobfs
+#PBS -l walltime=6:00:00,mem=6GB,jobfs=50GB,other=pernodejobfs
 #PBS -l wd
 #PBS -l storage=gdata/v10+scratch/v10+gdata/if87+gdata/fj7+scratch/fj7+scratch/u46+gdata/u46
 #PBS -l ncpus=18 # This value should be $END-$START+1
--- s2_l1/feb_level1.sh
+++ s2_l1/feb_level1.sh
@@ -4,13 +4,13 @@
 #PBS -P u46
 #PBS -W umask=017
 #PBS -q normal
-#PBS -l walltime=6:00:00,mem=4GB,jobfs=50GB,other=pernodejobfs
+#PBS -l walltime=6:00:00,mem=6GB,jobfs=50GB,other=pernodejobfs
 #PBS -l wd
 #PBS -l storage=gdata/v10+scratch/v10+gdata/if87+gdata/fj7+scratch/fj7+scratch/u46+gdata/u46
 #PBS -l ncpus=29 # This value should be $END-$START+1
 
 
-INPUT_DIR='/g/data/fj7/Copernicus/Sentinel-2/MSI/L1C/2019/2019-12/'
+INPUT_DIR='/g/data/fj7/Copernicus/Sentinel-2/MSI/L1C/2020/2020-02/'
 OUTPUT_DIR='/g/data/v10/AGDCv2/indexed_datasets/cophub/s2/s2_l1c_yamls'
 
 START=1
