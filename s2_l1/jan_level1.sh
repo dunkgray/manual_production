cc05dcbaa2f6eddc7bf7bfa15690d2d2996e1268
Duncan Gray <duncan.gray@ga.gov.au>
2020-04-06 10:32:54 +1000

    update mem and wall time

--- s2_l1/jan_level1.sh
+++ s2_l1/jan_level1.sh
@@ -4,7 +4,7 @@
 #PBS -P u46
 #PBS -W umask=017
 #PBS -q normal
-#PBS -l walltime=6:00:00,mem=6GB,jobfs=50GB,other=pernodejobfs
+#PBS -l walltime=4:00:00,mem=10GB,jobfs=50GB,other=pernodejobfs
 #PBS -l wd
 #PBS -l storage=gdata/v10+scratch/v10+gdata/if87+gdata/fj7+scratch/fj7+scratch/u46+gdata/u46
 #PBS -l ncpus=15
