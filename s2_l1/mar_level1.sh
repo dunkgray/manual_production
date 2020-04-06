f13d04ac8fc99b53639ed98757723ff4c6f776ca
Duncan Gray <duncan.gray@ga.gov.au>
2020-04-06 11:17:06 +1000

    update memory request

--- s2_l1/mar_level1.sh
+++ s2_l1/mar_level1.sh
@@ -4,11 +4,12 @@
 #PBS -P u46
 #PBS -W umask=017
 #PBS -q normal
-#PBS -l walltime=4:00:00,mem=10GB,jobfs=50GB,other=pernodejobfs
+#PBS -l walltime=4:00:00,mem=155GB,other=pernodejobfs
 #PBS -l wd
 #PBS -l storage=gdata/v10+scratch/v10+gdata/if87+gdata/fj7+scratch/fj7+scratch/u46+gdata/u46
 #PBS -l ncpus=31
 # This value should be $END-$START+1
+# the mem is ncpu * 5 GB
 
 
 INPUT_DIR='/g/data/fj7/Copernicus/Sentinel-2/MSI/L1C/2020/2020-03/'
