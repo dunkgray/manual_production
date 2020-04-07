231f41e52dbe88d0c737fb8b06f78c4bb14ccb9d
Duncan Gray <duncan.gray@ga.gov.au>
2020-04-07 15:40:59 +1000

    level 2 code

---
+++ s2_l2/comparison_go_execute_s2ard.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+loc=$PWD
+RUNDIR='/g/data/v10/work/s2_ard/pbs/level1'
+S2L1DIR='/g/data/fj7/Copernicus/Sentinel-2/MSI/L1C/2019/'
+LOGDIR='/g/data/v10/work/s2_ard/wagl'
+WORKDIR='/g/data/if87/workdir'
+OUTPUT='/g/data/if87/datacube/002/S2_MSI_ARD/packaged'
+ENV=$loc/definitive.env
+
+
+#TEST=False
+TEST=true
+
+if [ "$TEST" = true ] ; then
+  echo 'Using test directories'
+  S2L1DIR='/g/data/fj7/Copernicus/Sentinel-2/MSI/L1C/2019/2019-11/20S125E-25S130E/'
+  LOGDIR=$loc/logdir/
+  RUNDIR=$LOGDIR
+  WORKDIR=$loc/workdir/
+  OUTPUT=$loc/pkgdir/
+  ENV=$loc/definitive.env
+fi
+
+./execute_s2ard \
+    --project u46 \
+    --level1-dir $S2L1DIR \
+    --workdir $WORKDIR \
+    --logdir $LOGDIR \
+    --output-dir $OUTPUT \
+    --copy-parent-dir-count 1 \
+    --file-mod-start  2019-11-04 \
+    --file-mod-end  2019-11-05 \
+    --task test \
+    --rundir $RUNDIR \
+    --env  $ENV\
+
---
+++ s2_l2/execute_s2ard_191206.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+loc=$PWD
+RUNDIR='/g/data/v10/work/s2_ard/pbs/level1'
+S2L1DIR='/g/data/fj7/Copernicus/Sentinel-2/MSI/L1C/2019/2019-12'
+LOGDIR='/g/data/v10/work/s2_ard/wagl'
+WORKDIR='/g/data/if87/workdir'
+OUTPUT='/g/data/if87/datacube/002/S2_MSI_ARD/packaged'
+ENV=$loc/definitive.env
+
+./execute_s2ard \
+    --project u46 \
+    --level1-dir $S2L1DIR \
+    --workdir $WORKDIR \
+    --logdir $LOGDIR \
+    --output-dir $OUTPUT \
+    --copy-parent-dir-count 1 \
+    --file-mod-start  2019-12-06 \
+    --file-mod-end  2019-12-14 \
+    --task test \
+    --rundir $RUNDIR \
+    --env  $ENV\
+
